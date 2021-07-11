e6c582fd83e5ab672551d8c578accaae3eca8dcf
yxf965 <yxf965@cs.bham.ac.uk>
2021-07-12 05:54:07 +0900

    a

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/yujif38/Django_project_pythonanywhere'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
